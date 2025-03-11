9502230245716f151972dbfc7110d83f6b8f6d9c
deep-ydv <work.pradeepydv@gmail.com>
2025-03-12 01:23:27 +0530

    First Approach To connect Database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+/* FIRST APPROACH --------------------------
+import mongoose from "mongoose"
+import { DB_NAME } from "./constants"
 
+import express from "express"
+
+const app=express()
+
+//using immediate function we can connect database
+( async ()=>{
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) // syntax to connect databse
+    app.on("error",(error)=>{
+      console.log("ERRR: ", error);
+      throw error;
+    })
+
+    app.listen(process.env.PORT,()=>{
+      console.log(`App is listening on port ${process.env.PORT}`);
+    })
+
+  } catch (error) {
+    console.log("ERROR: ", error);
+    throw error;
+  }
+})() //call immediate function
+------------------------------------------------------------------------------------- */ 
